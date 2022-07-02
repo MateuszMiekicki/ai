fd1e3285ca3d7300e3da97bc99b39cd967a879a8
Mateusz Miekicki <miekickimateusz@gmail.com>
2022-07-02 10:29:38 +0200

    Added generator for items

---
+++ include/knapsack/knapsack/generate/Generate.hh
@@ -0,0 +1,63 @@
+#ifndef KNAPSACK_GENERATE_HH
+#define KNAPSACK_GENERATE_HH
+#include "Item.hh"
+#include "Number.hh"
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <string>
+namespace knapsack
+{
+class Generate
+{
+    static auto &randomEngine()
+    {
+        std::random_device randomDevice;
+        thread_local std::mt19937_64 engine(randomDevice());
+        return engine;
+    }
+
+    static auto draw(long double max)
+    {
+        std::uniform_real_distribution<long double> uniformRealDistribution(0.0L, max);
+        return (uniformRealDistribution(randomEngine()));
+    }
+
+  public:
+    static items_t generate(const Weight maxWeight, const Value maxValue, std::size_t how)
+    {
+        auto items = items_t();
+        items.reserve(how);
+        for (auto i = how; i not_eq 0; --i)
+        {
+            items.emplace_back(draw(maxValue), draw(maxWeight));
+        }
+        return items;
+    }
+
+    static void write(std::ofstream &&file, const items_t &items)
+    {
+        for (auto &&item : items)
+        {
+            file << item.value() << ' ' << item.weight() << '\n';
+        }
+    }
+
+    static items_t read(std::ifstream &&file)
+    {
+        items_t items;
+        std::string line;
+        while (std::getline(file, line))
+        {
+            if (auto found = line.find_first_of(";"); found not_eq std::string::npos)
+            {
+                const auto value = std::stold(std::string(std::begin(line), std::begin(line) + found));
+                const auto weight = std::stold(std::string(std::begin(line) + found+1, std::end(line)));
+                items.emplace_back(value, weight);
+            }
+        }
+        return items;
+    }
+};
+} // namespace knapsack
+#endif // KNAPSACK_GENERATE_HH
